58e4180a513337273ca140a23da970e0860f496a
kyoko <kyoko1093@gmail.com>
2017-12-02 23:25:41 +0900

    initial commit

---
+++ app/src/main/java/com/phicdy/musicplayer/SongListActivity.kt
@@ -0,0 +1,12 @@
+package com.phicdy.musicplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SongListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+    }
+}
