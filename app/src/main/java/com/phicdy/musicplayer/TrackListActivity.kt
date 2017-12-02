0963cba9c9a9b2a1f927dfc1b966a7b9930960a3
kyoko <kyoko1093@gmail.com>
2017-12-03 00:02:50 +0900

    rename files
    song -> track

--- app/src/main/java/com/phicdy/musicplayer/TrackListActivity.kt
+++ app/src/main/java/com/phicdy/musicplayer/TrackListActivity.kt
@@ -3,10 +3,10 @@ package com.phicdy.musicplayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SongListActivity : AppCompatActivity() {
+class TrackListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_song_list)
+        setContentView(R.layout.activity_track_list)
     }
 }
